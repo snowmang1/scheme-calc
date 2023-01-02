13650bcfc7413f28466d26bcbdda4e5e6885da16
Evan Drake <evanmdrake@icloud.com>
2023-01-02 12:15:02 -0800

    tested file streams, and string parsing

---
+++ read_file.scm
@@ -0,0 +1,15 @@
+
+;; open file port
+(define in_file (open-input-file "test_file.txt"))
+
+(define* (explode)
+	;; get next char
+	(define c (read-char in_file))
+	(cond
+		((not (eof-object? c))
+		(display c)
+		(explode))
+	)
+)
+
+(explode)
